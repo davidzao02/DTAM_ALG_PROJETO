9176e2f1f037f78628f564be4cf04c4ce9fa22dd
davidzao02 <david.alexandre.costa.santos@gmail.com>
2021-01-03 18:31:47 +0000

    Peças e Alterações Código

--- PROJETO_DAMAS/jogar.js
+++ PROJETO_DAMAS/jogar.js
@@ -2,3 +2,16 @@ let firstName = prompt('Insert first player name!')
 
 let secondName = prompt('Insert second player name!')
 
+let pointsFirst = 0, pointsSecond = 0
+
+let winner
+
+if (pointsFirst == 12 || pointsSecond == 12){
+    if (pointsFirst == 12) {
+        winner = firstName
+    }else if (pointsSecond == 12){
+        winner = secondName
+    }
+    alert(`FIM DO JOGO! O VENCEDOR É ${winner}`)
+}
+
