6acc712c35a636b8469ceb71121d552ab7270b6a
davidzao02 <david.alexandre.costa.santos@gmail.com>
2021-01-31 17:19:02 +0000

    alteracoes

--- PROJETO_DAMAS/play.js
+++ PROJETO_DAMAS/play.js
@@ -27,7 +27,7 @@ function fillTable(map) {
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
             if (map[i][j] == 1) {
-
+                
             }
         }
     }
